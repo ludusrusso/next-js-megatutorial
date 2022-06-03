d303d08b6c0783504fda6cec102bc4dc8e4f9c23
Ludovico Russo <ludus.russo@gmail.com>
2022-06-03 16:46:16 +0200

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,14 @@
-import { InferGetStaticPropsType } from "next";
 import { Footer } from "../components/footer";
 import { Nav } from "../components/nav";
 
-type HomeProps = InferGetStaticPropsType<typeof getStaticProps>;
-
-const Home = ({ renderedTime }: HomeProps) => {
+const Home = () => {
   return (
     <div className="">
       <Nav />
       <h1> Home </h1>
-      <p>Renderizzato in data: {renderedTime}</p>
       <Footer />
     </div>
   );
 };
 
 export default Home;
-
-export const getStaticProps = async () => {
-  const renderedTime = new Date();
-  return {
-    props: {
-      renderedTime: renderedTime.toISOString(),
-    },
-    revalidate: 10,
-  };
-};
