7a45ccc37849fc9dd3e5f8ab40db005d3223be58
Ludovico Russo <ludus.russo@gmail.com>
2022-06-03 16:44:18 +0200

    fix button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,28 @@
+import { InferGetStaticPropsType } from "next";
 import { Footer } from "../components/footer";
 import { Nav } from "../components/nav";
 
-const Home = () => {
+type HomeProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+const Home = ({ renderedTime }: HomeProps) => {
   return (
     <div className="">
       <Nav />
       <h1> Home </h1>
+      <p>Renderizzato in data: {renderedTime}</p>
       <Footer />
     </div>
   );
 };
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const renderedTime = new Date();
+  return {
+    props: {
+      renderedTime: renderedTime.toISOString(),
+    },
+    revalidate: 10,
+  };
+};
